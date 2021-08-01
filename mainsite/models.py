e3eea3ca0a43e938b1c1bc94b61f6e6517fb6d03
CHIINGTING <tad20077@gmail.com>
2021-08-01 18:28:40 +0800

    add html

--- mainsite/models.py
+++ mainsite/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
 class Post(models.Model):
-    title = models.CharField(max_length=200) # max 200 bit
+    # 繼承models.Model class
+    title = models.CharField(max_length=200)  # max 200 bit
     slug = models.CharField(max_length=200)
-    body = models.TextField() # unlimit size
+    body = models.TextField()  # unlimit size
     pub_date = models.DateTimeField(default=timezone.now())
 
     class Meta:
         ordering = ('-pub_date'),
-
+        db_table = 'articels'
 
     def __str__(self):
-        return self.title,self.slug
+       return self.body
--- mainsite/views.py
+++ mainsite/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from django.template.loader import get_template
+from datetime import datetime
+from .models import Post
+import templates
 # Create your views here.
+
+def homepage(request):
+    template = get_template('index.html')
+    posts = Post.objects.all()
+    now = datetime.now()
+    html = template.render(locals())
+
+#   posts = Post.objects.all() #資料庫全部撈出
+#  post_list =list()
+#    for count, post in enumerate(posts):
+#        post_list.append("No.{} ".format(str(count))+"<font color=red>"+str(post)+"</font>"+str(post.pub_date)+"<br>")
+#        post_list.append("<small>"+str(post.body)+"</small><br><br>")
+    return HttpResponse(html)
